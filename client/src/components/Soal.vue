015d96105c09a723f433732c2ce68e5fc7f62e74
agrha <agrhakartz@gmail.com>
2018-03-23 09:37:31 +0700

    vuex standard

--- client/src/components/Soal.vue
+++ client/src/components/Soal.vue
@@ -1,20 +1,43 @@
 <template>
   <div>
     <div class="jumbotron">
-      <h2>Soal ke-i</h2>
+      <h2>{{count}}</h2>
       <hr class="my-4">
-      <h3 class="display-4">Soal Untuk Diketik</h3>
+      <h3 class="display-4">{{kacang(count)}}</h3>
+      <input type = 'text' v-model='ketikan'>
+      <button @click=answer>jawab</button>
     </div>
     <Form></Form>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import Form from '@/components/Form'
 
 export default {
+  data () {
+    return {
+      count: 1,
+      ketikan: ''
+    }
+  },
+  methods: {
+    answer () {
+      if (this.ketikan === this.kacang(this.count)) {
+        this.count++
+      }
+    }
+  },
   components: {
     Form
+  },
+  computed: {
+    ...mapGetters([
+      'groups',
+      'jawaban',
+      'kacang'
+    ])
   }
 }
 </script>
