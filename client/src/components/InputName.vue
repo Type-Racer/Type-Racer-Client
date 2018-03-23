1b7f499adba418d524d7432d765472b1bbea473d
fzgitara <fachrizalgita@gmail.com>
2018-03-23 16:56:46 +0700

    apa ya

--- client/src/components/InputName.vue
+++ client/src/components/InputName.vue
@@ -8,9 +8,9 @@
     <div class="card-body">
       <h1 class="card-title">Typo-Racer</h1>
       <p class="card-text">Masukkan nama Anda</p>
-      <input id="form-nama" type="text" class="form"><br><br>
+      <input id="form-nama" type="text" class="form" v-on:keyup="keymonitor"><br><br>
       <router-link :to="{path: '/room'}">
-        <button @click="setName" class="btn btn-dark">Mulai</button>
+        <button @click="setName" class="btn btn-light">Mulai</button>
       </router-link>
     </div>
     <div class="card-footer text-muted">
@@ -27,6 +27,11 @@ export default {
   methods: {
     setName () {
       this.$store.commit('setPlayerName', document.querySelector('#form-nama').value)
+    },
+    keymonitor: function (event) {
+      if (event.key === 'Enter') {
+        this.setName()
+      }
     }
   }
 }
@@ -39,5 +44,10 @@ export default {
 }
 img{
   height: 100px;
+}
+.card-body {
+  color: white;
+  text-shadow: 2px 2px 4px black;
+  background: url(https://media3.giphy.com/media/6Z3D5t31ZdoNW/200.gif)
 }
 </style>
